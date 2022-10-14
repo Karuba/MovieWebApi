dd9ee09926c88c420e50f9d00df14b2fb2c9a653
Kirill <kirill.bnf@gmail.com>
2022-10-14 09:44:47 +0300

    add movie data context

---
+++ MovieWebApi.Infrastructure.Data/RepositoryContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieWebApi.Domain.Core.Entities;
+using MovieWebApi.Infrastructure.Data.Configurations;
+
+namespace MovieWebApi.Infrastructure.Data
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            model.ApplyConfiguration(new MovieConfiguration());
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
