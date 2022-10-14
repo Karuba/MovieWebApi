97afcc12db204a741d7fb1d22c928c0aa729ec0e
Kirill <kirill.bnf@gmail.com>
2022-10-14 09:44:03 +0300

    add movie configuration

---
+++ MovieWebApi.Infrastructure.Data/Configurations/MovieConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MovieWebApi.Domain.Core.Entities;
+
+namespace MovieWebApi.Infrastructure.Data.Configurations
+{
+    public class MovieConfiguration : IEntityTypeConfiguration<Movie>
+    {
+        public void Configure(EntityTypeBuilder<Movie> builder)
+        {
+            builder.HasData( 
+                new Movie
+                {
+                    Id = new Guid("01abbca8-664d-4b20-b5de-024705497d4a"),
+                    Name = "Pulp Fiction",
+                    Description = "This is film was made by Quentin Tarantino",
+                    Rating = null,
+                },
+                new Movie
+                {
+                    Id = new Guid("02abbca8-664d-4b20-b5de-024705497d4a"),
+                    Name = "Snatch",
+                    Description = "This is film was made by Guy Ritchie",
+                    Rating = 3,
+                }
+            );
+        }
+    }
+}
