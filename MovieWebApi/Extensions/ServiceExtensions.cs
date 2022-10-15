7b95b4636f324661f4806392b100fe25f363bdea
Kirill <kirill.bnf@gmail.com>
2022-10-15 12:50:49 +0300

    add migration, place for remote migration, seperate extensions

---
+++ MovieWebApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieWebApi.Domain.Interfaces.Repositories;
+using MovieWebApi.Infrastructure.Business;
+using MovieWebApi.Infrastructure.Data;
+using MovieWebApi.Infrastructure.Data.Repositories;
+using MovieWebApi.Infrastructure.Migr.SqlServer;
+using MovieWebApi.Services.Interfaces;
+
+namespace MovieWebApi.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        public static void ConfigureServiceManager(this IServiceCollection services) =>
+            services.AddScoped<IServiceManager, ServiceManager>();
+        public static void ConfigureDatabaseContext(this IServiceCollection services,
+            IConfiguration configuration) =>
+            //services.AddDbContext<RepositoryContext>(options =>
+            //        options.UseSqlServer(configuration.GetConnectionString("SqlServer"), b =>
+            //            b.MigrationsAssembly("MovieWebApi")));
+                ConnectionExtension.ConfigureDatabaseContext(services, configuration);
+        public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+        //public static void ConfigureExceptionHandlerMiddleware(this IServiceCollection services) =>
+        //    services.AddTransient<ExceptionHandlingMiddleware>();
+    }
+}
