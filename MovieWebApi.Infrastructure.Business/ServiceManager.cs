568003e021b24d23dc32351fd7602f1976fc8e47
Kirill <kirill.bnf@gmail.com>
2022-10-15 12:48:52 +0300

    upgrade services

--- MovieWebApi.Infrastructure.Business/ServiceManager.cs
+++ MovieWebApi.Infrastructure.Business/ServiceManager.cs
@@ -1,7 +1,17 @@
-﻿namespace MovieWebApi.Infrastructure.Business
-{
-    public class ServiceManager
-    {
+﻿using AutoMapper;
+using MovieWebApi.Domain.Interfaces.Repositories;
+using MovieWebApi.Infrastructure.Business.Services;
+using MovieWebApi.Services.Interfaces;
 
+namespace MovieWebApi.Infrastructure.Business
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<IMovieService> _lazyMovieService;
+        public ServiceManager(IRepositoryManager repository, IMapper mapper)
+        {
+            _lazyMovieService = new Lazy<IMovieService>(() => new MovieService(repository, mapper));
+        }
+        public IMovieService movieService => _lazyMovieService.Value;
     }
 }
--- MovieWebApi.Infrastructure.Business/Services/MovieService.cs
+++ MovieWebApi.Infrastructure.Business/Services/MovieService.cs
@@ -9,7 +9,7 @@ using MovieWebApi.Services.Interfaces;
 
 namespace MovieWebApi.Infrastructure.Business.Services
 {
-    internal class MovieService : IMovieService
+    public class MovieService : IMovieService
     {
         private readonly IRepositoryManager _repository;
         private readonly IMapper _mapper;
@@ -17,6 +17,27 @@ namespace MovieWebApi.Infrastructure.Business.Services
         {
             _repository = repository;
             _mapper = mapper;
+        }
+
+        public async Task<MovieDto> CreateMovie(MovieCreateDto movieData)
+        {
+            var movie = _mapper.Map<Movie>(movieData);
+            _repository.Movie.AddMovie(movie);
+            await _repository.SaveAsync();
+
+            var movieToReturn = _mapper.Map<MovieDto>(movie);
+
+            return movieToReturn;
+        }
+
+        public async Task DeleteMovie(Guid id)
+        {
+            var movie = await _repository.Movie.GetMovieAsync(id);
+            if (movie is null)
+                throw new NotFoundException($"Movie with id: {id} doesn't exist in the database");
+
+            _repository.Movie.DeleteMovie(movie);
+            await _repository.SaveAsync();
         }
 
         public async Task<MovieDto> GetMovieAsync(Guid id)
@@ -45,5 +66,6 @@ namespace MovieWebApi.Infrastructure.Business.Services
 
             return _mapper.Map<MovieDto>(movie);
         }
+
     }
 }
